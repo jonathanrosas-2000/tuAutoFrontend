4fc8827a394ac99cbf975d665f7fb0c6ed5d446b
JRosas <81265874+jonathanrosas-2000@users.noreply.github.com>
2024-11-11 22:36:44 -0600

    Adding Image carrousel

--- src/components/products/productcontainer/ProductContainer.tsx
+++ src/components/products/productcontainer/ProductContainer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import ProductCard from "../pricecard/PriceCard";
 import Summary from "../summary/Summary";
@@ -10,9 +11,11 @@ import Image from "next/image";
 import { SeoMeta, SimilarCars } from "@/components";
 import { carType } from "@/types/typing";
 import SummaryHighlights from "../summaryhighlights/SummaryHighlights";
+import ImageCarrousel from "../../carrousel/ImageCarousel";
 
 const ProductContainer = ({ product }: { product: carType }) => {
-  const { name, year, description, images, mileage, extra, price, highlights } = product;
+  const { name, year, description, images, mileage, extra, price, highlights } =
+    product;
   const {
     title,
     description: metaDescription,
@@ -21,31 +24,48 @@ const ProductContainer = ({ product }: { product: carType }) => {
     productSchema,
   } = product.seoDetails;
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <>
-      <SeoMeta 
-        title={title} 
-        description={metaDescription} 
+      <SeoMeta
+        title={title}
+        description={metaDescription}
         keywords={keywords}
         subject={subject}
         productSchema={productSchema}
       />
       <div className={styles.container}>
         <div className={styles.car}>
-          <div className={styles.imageContainer}>
-            {images.length > 0 ? (
-              <Image
-                key={images[0]}
-                src={`${images[0]}`}
-                alt={`Carro seminuevo ${name}`}
-                width={500}
-                height={400}
-                className={styles.image}
-              />
+          {/* <div className={styles.imageContainer}> */}
+          <ImageCarrousel images={images} altText="Algo" />
+          {/* {images.length > 0 ? (
+              <div className={styles.carousel}>
+                <Image
+                  key={images[currentIndex]}
+                  src={images[currentIndex]}
+                  alt={`Carro seminuevo ${name}`}
+                  width={1200}
+                  height={1200}
+                  className={styles.image}
+                />
+                <button className={styles.prevButton} onClick={prevImage}>Anterior</button>
+                <button className={styles.nextButton} onClick={nextImage}>Siguiente</button>
+              </div>
             ) : (
               <h2>No image</h2>
-            )}
-          </div>
+            )} */}
+          {/* </div> */}
           <Summary
             year={year}
             version={"1"}
